93b05562e79029bfae03414d3ad1e94500a03bdf
Tessa <tessakkelly@gmail.com>
2015-01-24 12:03:44 -0800

    add search

--- dictTrie.js
+++ dictTrie.js
@@ -3,22 +3,29 @@ module.exports = function() {
     this.value = letter || null;
     this.children = {};
     this.isWord = false;
-  }
+  };
 
   Node.prototype.addLetter = function(letter) {
     if(!this.children[letter]) {
       this.children[letter] = new Node(letter);
     }
     return this.children[letter];
-  }
+  };
 
   Node.prototype.setAsWord = function(isWord) {
     this.isWord = isWord;
-  }
+  };
+
+  Node.prototype.getLetter = function(letter, endInd) {
+    if(this.children[letter] && this.children[letter].isWord && endInd) {
+      return true;
+    }
+    return this.children[letter]
+  };
 
   var Trie = function() {
     this.root = new Node();
-  }
+  };
 
   Trie.prototype.addWord = function(word, node, ind) {
     node = node || this.root;
@@ -30,7 +37,23 @@ module.exports = function() {
       return;
     }
     return this.addWord(word, nextNode, ind+1);
-  }  
+  };
+
+  Trie.prototype.search = function(word, node, ind) {
+    node = node || this.root;
+    ind = ind || 0;
+
+    var wordInd = (ind == word.length - 1) ? true : false;
+    var nextNode = node.getLetter(word[ind], wordInd);
+
+    if(nextNode === true && ind == word.length - 1) {
+      return true;
+    } else if(nextNode && ind < word.length - 1) {
+      return this.search(word, nextNode, ind+1)
+    } else {
+      return false;
+    }
+  };
 
   return {
     Trie: Trie,
--- spec/trie-spec.js
+++ spec/trie-spec.js
@@ -22,6 +22,21 @@ describe("Node", function(argument) {
       expect(this.nodeB.isWord).toBe(false);
     }) 
   })
+
+  describe("getLetter", function() {
+    it("returns true if letter finishes a word", function() {
+      this.nodeB.setAsWord(true);
+      expect(this.node.getLetter("b", true)).toEqual(true);
+    })
+
+    it("returns the letter node if letter doesn't finish a word", function() {
+      expect(this.node.getLetter("b")).toEqual(this.nodeB);
+    })
+
+    it("returns undefined if letter is not found", function() {
+      expect(this.node.getLetter("c")).toEqual(undefined);
+    })
+  })
 })
 
 describe("Trie", function() {
@@ -49,6 +64,30 @@ describe("Trie", function() {
 
     it("updates word status of last letter of word", function() {
       expect(this.tree.root.children.h.children.e.children.l.children.l.children.o.isWord).toBe(true)
+    })
+  })
+
+  describe("search", function() {
+    beforeEach(function() {
+      this.tree.addWord("bananas");
+      this.tree.addWord("baracuda");
+    })
+
+    it("returns true if word is in tree", function() {
+      expect(this.tree.search("bananas")).toBe(true);
+      expect(this.tree.search("baracuda")).toBe(true);
+    })
+
+    it("returns false if word is not in tree", function() {
+      expect(this.tree.search("cat")).toBe(false);
+      expect(this.tree.search("banan")).toBe(false);
+      expect(this.tree.search("ba")).toBe(false);
+    })
+
+    it("continues to search even if sub-word is found", function() {
+      this.tree.addWord("ban");
+      expect(this.tree.search("ban")).toBe(true);
+      expect(this.tree.search("bana")).toBe(false);
     })
   })
 })
